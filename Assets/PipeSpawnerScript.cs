195ffabb4eb0956cc8613cfb954ae1e82bc6773e
ChavinduShirantha <chavindushirantha@gmail.com>
2024-04-21 10:53:20 +0530

    Created pipe spawning function

---
+++ Assets/PipeSpawnerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnerScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 3;
+    private float timer = 0;
+    public float heighOffset = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }    
+    }
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heighOffset;
+        float highestPoint = transform.position.y + heighOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+    }
+}
